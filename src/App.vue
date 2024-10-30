5d329facae82b9ba05d1df07ae49b2e1f2a90296
Александров Михаил Андреевич <m.aleksandrov@nicetu.spb.ru>
2024-10-30 11:09:53 +0300

    hello

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     1234
+    <PersonsPage></PersonsPage>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import PersonsPage from './components/PersonsPage'
 </script>
 
 <style scoped>
