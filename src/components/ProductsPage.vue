4763267739e6d788ceaf66bff022dada855e7728
Александров Михаил Андреевич <m.aleksandrov@nicetu.spb.ru>
2024-11-05 12:05:15 +0300

    did products list and products adding logic

--- src/components/ProductsPage.vue
+++ src/components/ProductsPage.vue
@@ -1,48 +1,59 @@
-    <template>
-        <h1>Продукты</h1>
-        <h2>Добавьте продукты, цену которых нужно разделить</h2>
-        <div v-for="product in products" :key="product.name">
-            {{ product.name }} - {{ product.price }}
-        </div>
+<template>
+	<h1>Продукты</h1>
+	<h2>Добавьте продукты, цену которых нужно разделить</h2>
+	<div v-for="product in products" :key="product.name">
+		{{ product.name }} - {{ product.price }}
+		<div v-for="person in persons" :key="person.name">
+			<button @click="selectProduct(person, product)">
+				{{ person.name }}
+			</button>
+		</div>
+	</div>
 
-        <div>
-            <input type="text" v-model="productName">
-            <input type="text" v-model="productPrice">
-            <button @click="addNewProductAndClearInput">+</button>
-        </div>
-
-    </template>
+        <button @click="formVisible = true" v-if="formVisible === false">
+            +
+        </button>
+	<div v-if="formVisible === true">
+		<input type="text" v-model="productName" />
+		<input type="text" v-model="productPrice" />
+		<button @click="addNewProductAndClearInput">+</button>
+	</div>
+</template>
 
 <script setup lang="ts">
-import usePersonsAndProductsStore, { type IPerson, type IProduct } from '@/stores/usePersonsAndProductsStore';
-import { computed, ref } from 'vue';
+import usePersonsAndProductsStore, {
+	type IPerson,
+	type IProduct,
+} from "@/stores/usePersonsAndProductsStore";
+import { computed, ref } from "vue";
 
 const store = usePersonsAndProductsStore();
 const persons = computed(() => store.persons);
-const products = computed(()=> store.products);
+const products = computed(() => store.products);
 
-
-const productName = ref<string>('');
+const productName = ref<string>("");
 const productPrice = ref<number>();
-let formVisible = ref<boolean>(false)
+let formVisible = ref<boolean>(false);
 
-function personsToNameArray(objArray: IPerson[]){
-    return objArray.map((obj)=> obj.name)
+function personsToNameArray(objArray: IPerson[]) {
+	return objArray.map((obj) => obj.name);
 }
-function addNewProductAndClearInput(){
-    const newProduct: IProduct = {
-        name: productName.value,
-        price: Number(productPrice.value),
-        selectedBy: [],
-        paidBy: null
-    }
-    store.addNewProduct(newProduct);
-    productName.value = '';
-    productPrice.value = 0;
-    formVisible.value = false;
+function addNewProductAndClearInput() {
+	const newProduct: IProduct = {
+		name: productName.value,
+		price: Number(productPrice.value),
+		selectedBy: [],
+		paidBy: null,
+	};
+	store.addNewProduct(newProduct);
+	productName.value = "";
+	productPrice.value = 0;
+	formVisible.value = false;
+}
+function selectProduct(person: IPerson, product: IProduct) {
+	person.selectedProducts.push(product.name);
+	product.selectedBy.push(person.name);
 }
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
