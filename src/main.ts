051df1a8ff1c9d031b10ea8478acba96d629e044
Александров Михаил Андреевич <m.aleksandrov@nicetu.spb.ru>
2024-11-05 12:03:03 +0300

    add vuetify to project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from "./App.vue";
 
 const pinia = createPinia();
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
 const app = createApp(App);
 
 app
+    .use(vuetify)
     .use(pinia)
     .mount("#app");
