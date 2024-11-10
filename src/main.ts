f25b5aa0bdd53ee1c911d76c68189690159d36a2
Mikhail Alexandrov <mikesperanskii@gmail.com>
2024-11-10 18:57:43 +0300

    created router and add it to main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import router from "./router";
 
 import App from "./App.vue";
+
 
 const pinia = createPinia();
 const vuetify = createVuetify({
@@ -18,4 +20,5 @@ const app = createApp(App);
 app
     .use(vuetify)
     .use(pinia)
+    .use(router)
     .mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,33 @@
-// import { createRouter, createWebHistory } from 'vue-router'
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'home',
-//       component: HomeView
-//     },
-//     {
-//       path: '/about',
-//       name: 'about',
-//       // route level code-splitting
-//       // this generates a separate chunk (About.[hash].js) for this route
-//       // which is lazy-loaded when the route is visited.
-//       component: () => import('../views/AboutView.vue')
-//     }
-//   ]
-// })
+import PersonsPage from "@/pages/PersonsPage.vue";
+import ProductsPage from "@/pages/ProductsPage.vue";
+import ResultPage from "@/pages/ResultPage.vue";
+import StartPage from "@/pages/StartPage.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-// export default router
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "startPage",
+            component: StartPage,
+        },
+        {
+            path: '/persons',
+            name: 'personsPage',
+            component: PersonsPage
+        },
+        {
+            path: "/products",
+            name: "productsPage",
+            component: ProductsPage,
+        },
+        {
+            path: "/result",
+            name: "resultPage",
+            component: ResultPage,
+        },
+    ],
+});
+
+export default router;
