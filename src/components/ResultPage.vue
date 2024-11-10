c312f282b2f848d676a6ddb78eb2986bd838056c
Mikhail Alexandrov <mikesperanskii@gmail.com>
2024-11-10 18:30:26 +0300

    add debt logic to ResultPage

--- src/components/ResultPage.vue
+++ src/components/ResultPage.vue
@@ -1,13 +1,39 @@
 <template>
-	<h1>Итоги</h1>
-	<PersonList :persons-array="result" />
+    <h1>Итоги</h1>
+    <div class="debt__list">
+        <h2>Кто кому должен?</h2>
+        <div v-for="person in debts" :key="person.count">
+            <div>
+                {{ person.name }} должен ...
+                <div v-if="person.debts.length > 0">
+                    <div v-for="debt in person.debts" :key="debt.personName">
+                        {{ debt.personName }} - {{ debt.amount }}
+                    </div>
+                </div>
+                <div v-else>а ничего и не должен!</div>
+            </div>
+        </div>
+    </div>
+    <div class="debt__target">
+        <h2>Детализация долгов</h2>
+        <div v-for="person in debts">
+            <div v-if="person.debts.length > 0">
+                {{ person.name }} должен за
+                <div v-for="product in person.selectedProducts" :key="product">
+                    {{ product }}
+                </div>
+                <div v-for="debt in person.debts" :key="debt.personName">
+                    {{ debt.amount }} - {{ debt.personName }}
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
 import { useResults } from "@/composables/useResults";
-import PersonList from "./PersonList.vue";
 
-const { result } = useResults();
+const { debts } = useResults();
 </script>
 
 <style scoped lang="scss"></style>
