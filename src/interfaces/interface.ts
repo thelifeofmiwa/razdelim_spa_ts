90f1086f60dcbf5fb1ec814109f7a2c30fdd4495
Mikhail Alexandrov <mikesperanskii@gmail.com>
2024-11-10 16:58:59 +0300

    created IDebt interface for IUser

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -1,12 +1,18 @@
 export interface IPerson {
-    name: string,
-    count: number,
-    selectedProducts: string[]
+    name: string;
+    count: number;
+    selectedProducts: string[];
+    debts?: IDebt[];
 }
 
 export interface IProduct {
-    name: string,
-    price: number,
-    selectedBy: string[],
-    paidBy: string 
+    name: string;
+    price: number;
+    selectedBy: string[];
+    paidBy: string;
+}
+
+export interface IDebt {
+    personName: string;
+    amount: number;
 }
