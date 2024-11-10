77be2df839311b53b02a1b35fe057c0e6a35d016
Mikhail Alexandrov <mikesperanskii@gmail.com>
2024-11-10 18:58:10 +0300

    created StartPage component

---
+++ src/pages/StartPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="start">
+        <div class="content">
+            <header class="header">
+                <h1>Разделим!</h1>
+                <h2>Удобный способ разделить счёт в кофейне или кафе</h2>
+            </header>
+            <!-- Самая обычная HTML-вёрстка. Фактически, помимо текста, на странице отсутствует контент, поэтому было принято решение оставить её без декомпозиции.
+		 	 Потенциально, можно использовать пропсы для передачи контента тегов, но зачем усложнять то, что должно выглядеть просто? :) -->
+            <div class="description">
+                <p>
+                    "Разделим" - самый простой и приятный способ оставить от
+                    прошедшего вечера только приятные впечатления! Укажите
+                    <b>друзей</b> и <b>цену</b> позиции, а мы посчитаем всё за
+                    вас!
+                </p>
+            </div>
+            <div class="button">
+                <ProjectButton @click="$router.push('/persons')">Начать</ProjectButton>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import ProjectButton from '@/components/UI/ProjectButton.vue';
+</script>
+
+<style scoped lang="scss"></style>
